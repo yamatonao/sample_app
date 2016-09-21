029b3edc0cc1e2fd17d07478c0994afcc83263ee
yamatonao <yamatonao.net@gmail.com>
2016-09-21 16:53:59 +0900

    Finish 6.x series Yeah

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
 	def setup
-		@user = User.new(name: "Example User", email: "user@example.com")
+		@user = User.new(name: "Example User", email: "user@example.com",
+			password: "foobar", password_confirmation: "foobar")
 	end
 
 	test "should be valid" do
@@ -53,6 +54,23 @@ class UserTest < ActiveSupport::TestCase
 		duplicate_user.email = @user.email.upcase
 		@user.save
 		assert_not duplicate_user.valid?
+	end
+
+	test "email addresses should be saved as lower-case" do
+		mixed_case_email = "Foo@ExAMPle.CoM"
+		@user.email = mixed_case_email
+		@user.save
+		assert_equal mixed_case_email.downcase, @user.reload.email
+	end
+
+	test "password should be present (nonblank)" do
+		@user.password = @user.password_confirmation = " " *6
+		assert_not @user.valid?
+	end
+
+	test "password should habe a minimum length" do
+		@user.password = @user.password_confirmation = "a" * 5
+		assert_not @user.valid?
 	end
   # test "the truth" do
   #   assert true
