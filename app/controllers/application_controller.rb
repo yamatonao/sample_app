6a9ef7c926bd4afbfcef3847193db3fb0ada26b8
yamatonao <yamatonao.net@gmail.com>
2016-09-21 13:20:16 +0900

    Finish 4.x series

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-<<<<<<< HEAD
   	render html: "hello,world"
-=======
-  	render html: "hello world!"
->>>>>>> static-pages
   end
 end
