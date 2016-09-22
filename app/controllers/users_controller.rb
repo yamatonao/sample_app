c7d9339c978d579592914f553daa550ed026e139
yamatonao <yamatonao.net@gmail.com>
2016-09-22 16:41:22 +0900

    Finish 10.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit,:update]
 
 	def show
 		@user = User.find(params[:id])
@@ -12,6 +14,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			log_in @user
 			flash[:success] = "Welcome to the Sample App"
 			redirect_to @user
 		else
@@ -19,8 +22,33 @@ class UsersController < ApplicationController
 		end
 	end
 
-	private
-		def user_params
-			params.require(:user).permit(:name,:email,:password,:password_confirmation)
+	def edit
+	end
+
+	def update
+		if @user.update_attributes(user_params)
+			flash[:success] = "Profile updated"
+			redirect_to @user
+		else
+			render 'edit'
 		end
+	end
+
+private
+	def user_params
+		params.require(:user).permit(:name,:email,:password,:password_confirmation)
+	end
+
+	def logged_in_user
+		unless logged_in?
+			store_location
+			flash[:danger] = "Please log in."
+			redirect_to login_url
+		end
+	end
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_url) unless current_user?(@user)
+	end
 end
