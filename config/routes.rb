d82818c63f423b0bf1b2994b3cf29afceb4fe3b3
yamatonao <yamatonao.net@gmail.com>
2016-09-23 17:18:01 +0900

    Finish 12.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
@@ -10,6 +9,7 @@ Rails.application.routes.draw do
   delete 'logout', to:'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only:[:new,:create,:edit,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#hello'
 end
