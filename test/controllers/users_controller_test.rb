1ca6992f3573f21219e52e1fcc32b1ece3a2c384
yamatonao <yamatonao.net@gmail.com>
2016-09-23 10:38:29 +0900

    Finish 10.x series

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 		@other_user = users(:archer)
 	end
 
+	test "should redirect index when not logged in" do
+		get users_path
+		assert_redirected_to login_url
+	end
+
 	test "should redirect edit when not logged in" do
 		patch user_path(@user),params: {user:{
 			name:@user.name,
@@ -39,6 +44,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 			email: @user.email
 			}}
 		assert flash.empty?
+		assert_redirected_to root_url
+	end
+
+	test "should redirect destroy when not logged in" do
+		assert_no_difference 'User.count' do
+			delete user_path(@user)
+		end
+		assert_redirected_to login_url
+	end
+
+	test "should redirect destroy when logged in as a non-admin" do
+		log_in_as(@other_user)
+		assert_no_difference 'User.count' do
+			delete user_path(@user)
+		end
 		assert_redirected_to root_url
 	end
 end
