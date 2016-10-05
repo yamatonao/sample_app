c09f7ae5f31205a662d0db26fa5a78e288fb97f4
yamatonao <yamatonao.net@gmail.com>
2016-10-05 09:26:03 +0900

    Finish chapter 13

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,10 +4,9 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
-  validates :picture_size
+  validate  :picture_size
 
   private
-
   	def picture_size
   		if picture.size > 5.megabytes
   			error.add(:picture, "should be less than 5MB")
